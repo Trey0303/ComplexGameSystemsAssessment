bbb11adad39a3b4c387a6e2d155cc5267981454b
Trey Gleason <s209058@student.aie.edu>
2022-01-26 17:03:29 -0800

    working on skillCreator

--- c#/ComplexGameSystemRPGSkillCreator/Assets/Scripts/SkillCreator.cs
+++ c#/ComplexGameSystemRPGSkillCreator/Assets/Scripts/SkillCreator.cs
@@ -1,45 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public struct SkillData
+[System.Serializable]
+public class SkillData
 {
-    string name;
-    string type;
-    int damage;
-    float range;
-    int max_level;
+    public string name;
+    public string type;
+    public int damage;
+    public float range;
+    public int max_level;
+    public int skillLvl;
+
+    public SkillData(string nameString, string typeString, int damageInt, float rangeFloat, int max_levelInt, int skillLvlInt)
+    {
+        nameString = name;
+        typeString = type;
+        damageInt = damage;
+        rangeFloat = range;
+        max_levelInt = max_level;
+        skillLvlInt = skillLvl;
+
+    }
 
 }
 
 public class SkillCreator : MonoBehaviour
 {
+    int exp;
+    int max_exp;
+
+    //butttons to call functions
+    public Button createButton;
+    public Button editButton;
+    public Button deleteButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        exp = 1;
+    }
+
+    public void CreateSkill()
+    {
+        Debug.Log("Create skill");
+        //CreateSkill(name, type, damage, range, max_level);
     }
 
     //create skill using given parameters
-    void CreateSkill(string name, string type, int damage, float range, int max_level)
+    public void CreateSkill(string name, string type, int damage, float range, int max_level)
     {
         //create an empty skill
-
-        //overwrite skill with current parameters
 
         //save skill to a list of skills
     }
 
+    public void DeleteSkill()
+    {
+        Debug.Log("Delete skill");
+        DeleteSkill(name);
+    }
+
     //delete skill by name
-    void Delete(string name)
+    public void DeleteSkill(string name)
     {
         //check if skill exsists
         
         //if so then delete and remove from skill list
     }
 
+    public void EditSkill()
+    {
+        Debug.Log("Edit skill");
+        EditSkill(name);
+    }
+
     //adjust/change skills name, type, damage, range, or max_level
-    void EditSkill(string name)
+    public void EditSkill(string name)
     {
         //check if skills exsists
 
@@ -50,13 +88,24 @@ public class SkillCreator : MonoBehaviour
         //save change to skill list
     }
 
+    void LevelUpSkill()
+    {
+        LevelUpSkill(exp);
+    }
+
     //increase level of skill if conditions are met
-    void LevelUp(int exp)
+    void LevelUpSkill(int exp)
     {
         //check if skill has enough experience to level up
-
-        //if so then increase level
-
-        //check if there is any leftover experience to add after level up
+        if(exp >= max_exp)
+        {
+            //if so then increase level
+            //skillLvl = SkillData.skillLvl + 1;
+            if(exp > max_exp)
+            {
+                //check if there is any leftover experience to add after level up
+                exp = exp - max_exp;
+            }
+        }
     }
 }

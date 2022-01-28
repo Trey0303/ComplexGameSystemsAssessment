223cd892657b868b21a1884edc964af713f149d9
Trey Gleason <s209058@student.aie.edu>
2022-01-27 17:46:18 -0800

    working on skillCreator. added a way to create skill. working on a shop to purchase skills

--- c#/ComplexGameSystemRPGSkillCreator/Assets/Scripts/SaveSkillData.cs
+++ c#/ComplexGameSystemRPGSkillCreator/Assets/Scripts/SaveSkillData.cs
@@ -6,57 +6,57 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public class SaveSkillData : MonoBehaviour
 {
-    string currentName = "";
-    string currentType = "";
-    int currentDamage = 0;
-    float currentRange = 1.0f;
-    int currentMax_level = 1;
-    int currentSkillLvl = 1;
+    //string currentName = "";
+    //string currentType = "";
+    //int currentDamage = 0;
+    //float currentRange = 1.0f;
+    //int currentMax_level = 1;
+    //int currentSkillLvl = 1;
 
-    void Start()
-    {
-        SaveFile();
-        LoadFile();
-    }
+    //void Start()
+    //{
+    //    SaveFile();
+    //    LoadFile();
+    //}
 
-    public void SaveFile()
-    {
-        string destination = Application.persistentDataPath + "/save.dat";
-        FileStream file;
+    //public void SaveFile()
+    //{
+    //    string destination = Application.persistentDataPath + "/save.dat";
+    //    FileStream file;
 
-        if (File.Exists(destination)) file = File.OpenWrite(destination);
-        else file = File.Create(destination);
+    //    if (File.Exists(destination)) file = File.OpenWrite(destination);
+    //    else file = File.Create(destination);
 
-        SkillData data = new SkillData(currentName, currentType, currentDamage, currentRange, currentMax_level, currentSkillLvl);
-        BinaryFormatter bf = new BinaryFormatter();
-        bf.Serialize(file, data);
-        file.Close();
-    }
+    //    SkillData data = new SkillData(currentName, currentType, currentDamage, currentRange, currentMax_level, currentSkillLvl);
+    //    BinaryFormatter bf = new BinaryFormatter();
+    //    bf.Serialize(file, data);
+    //    file.Close();
+    //}
 
-    public void LoadFile()
-    {
-        string destination = Application.persistentDataPath + "/save.dat";
-        FileStream file;
+    //public void LoadFile()
+    //{
+    //    string destination = Application.persistentDataPath + "/save.dat";
+    //    FileStream file;
 
-        if (File.Exists(destination)) file = File.OpenRead(destination);
-        else
-        {
-            Debug.LogError("File not found");
-            return;
-        }
+    //    if (File.Exists(destination)) file = File.OpenRead(destination);
+    //    else
+    //    {
+    //        Debug.LogError("File not found");
+    //        return;
+    //    }
 
-        BinaryFormatter bf = new BinaryFormatter();
-        SkillData data = (SkillData)bf.Deserialize(file);
+    //    BinaryFormatter bf = new BinaryFormatter();
+    //    SkillData data = (SkillData)bf.Deserialize(file);
         
-        file.Close();
+    //    file.Close();
 
-        currentName = data.name;
-        currentType = data.type;
-        currentDamage = data.damage;
-        currentRange = data.range;
-        currentMax_level = data.maxLevel;
-        currentSkillLvl = data.skillLvl;
+    //    currentName = data.name;
+    //    currentType = data.type;
+    //    currentDamage = data.damage;
+    //    currentRange = data.range;
+    //    currentMax_level = data.maxLevel;
+    //    currentSkillLvl = data.skillLvl;
 
-        Debug.Log(data.name);
-    }
+    //    Debug.Log(data.name);
+    //}
 }
--- c#/ComplexGameSystemRPGSkillCreator/Assets/Scripts/SkillCreator.cs
+++ c#/ComplexGameSystemRPGSkillCreator/Assets/Scripts/SkillCreator.cs
@@ -3,94 +3,94 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[System.Serializable]
+//[System.Serializable]
 public class SkillData
 {
-    public string name;
-    public string type;
-    public int damage;
-    public float range;
-    public int maxLevel;
-    public int skillLvl;
+//    public string name;
+//    public string type;
+//    public int damage;
+//    public float range;
+//    public int maxLevel;
+//    public int skillLvl;
 
-    public SkillData(string nameString, string typeString, int damageInt, float rangeFloat, int max_levelInt, int skillLvlInt)
-    {
-        nameString = name;
-        typeString = type;
-        damageInt = damage;
-        rangeFloat = range;
-        max_levelInt = maxLevel;
-        skillLvlInt = skillLvl;
+//    public SkillData(string nameString, string typeString, int damageInt, float rangeFloat, int max_levelInt, int skillLvlInt)
+//    {
+//        nameString = name;
+//        typeString = type;
+//        damageInt = damage;
+//        rangeFloat = range;
+//        max_levelInt = maxLevel;
+//        skillLvlInt = skillLvl;
 
-    }
+//    }
 
-}
+//}
 
-public class SkillCreator : MonoBehaviour
-{
-    int exp;
-    int maxExp;
-    int damageStep;
-    int rangeStep;
-    int maxExpStep;
+//public class SkillCreator : MonoBehaviour
+//{
+//    int exp;
+//    int maxExp;
+//    int damageStep;
+//    int rangeStep;
+//    int maxExpStep;
 
-    //butttons to call functions
-    public Button createButton;
-    public Button editButton;
-    public Button deleteButton;
+//    //butttons to call functions
+//    public Button createButton;
+//    public Button editButton;
+//    public Button deleteButton;
 
-    //display menus
-    public GameObject createSkillMenu;
-    public GameObject editSkillMenu;
-    public GameObject deleteSkillMenu;
+//    //display menus
+//    public GameObject createSkillMenu;
+//    public GameObject editSkillMenu;
+//    public GameObject deleteSkillMenu;
 
-    //submits changes to skills
-    public Button submitCreate;
-    public Button submitEdit;
-    public Button submitDelete;
+//    //submits changes to skills
+//    public Button submitCreate;
+//    public Button submitEdit;
+//    public Button submitDelete;
 
-    //getting user input
-    InputField nameInput;
-    string myName;
-    InputField damageInput;
-    int myDamage;
-    InputField rangeInput;
-    int myRange;
+//    //getting user input
+//    InputField nameInput;
+//    string myName;
+//    InputField damageInput;
+//    int myDamage;
+//    InputField rangeInput;
+//    int myRange;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        exp = 1;
-        maxExp = 5;
-        damageStep = 3;
-        rangeStep = 2;
-        maxExpStep = 5;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        exp = 1;
+//        maxExp = 5;
+//        damageStep = 3;
+//        rangeStep = 2;
+//        maxExpStep = 5;
 
-        createSkillMenu.SetActive(false);
-        editSkillMenu.SetActive(false);
-        deleteSkillMenu.SetActive(false);
-    }
+//        createSkillMenu.SetActive(false);
+//        editSkillMenu.SetActive(false);
+//        deleteSkillMenu.SetActive(false);
+//    }
 
-    public void ShowCreateMenu()
-    {
-        Debug.Log("Create skill");
+//    public void ShowCreateMenu()
+//    {
+//        Debug.Log("Create skill");
 
-        //hide main buttons
-        createButton.gameObject.SetActive(false);
-        editButton.gameObject.SetActive(false);
-        deleteButton.gameObject.SetActive(false);
+//        //hide main buttons
+//        createButton.gameObject.SetActive(false);
+//        editButton.gameObject.SetActive(false);
+//        deleteButton.gameObject.SetActive(false);
 
-        //show skill creation ui
-        createSkillMenu.SetActive(true);
+//        //show skill creation ui
+//        createSkillMenu.SetActive(true);
 
 
-    }
+//    }
     
 
-    void LevelUpSkill()
-    {
-        //LevelUpSkill(exp);
-    }
+//    void LevelUpSkill()
+//    {
+//        //LevelUpSkill(exp);
+//    }
 
     //increase level of skill if conditions are met
     //void LevelUpSkill(int exp)

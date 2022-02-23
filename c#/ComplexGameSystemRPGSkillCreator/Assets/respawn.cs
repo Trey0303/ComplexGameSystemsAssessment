66bac6fc626632edfb977c48f47ac84154b43aa0
Trey Gleason <s209058@student.aie.edu>
2022-02-23 13:56:27 -0800

    added platforms for player to jump on

---
+++ c#/ComplexGameSystemRPGSkillCreator/Assets/respawn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    GameObject player;
+
+    GameObject spawnpoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        spawnpoint = GameObject.Find("respawn");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            player.GetComponent<KinematicBody>().enabled = false;
+            player.transform.position = spawnpoint.transform.position;
+            player.GetComponent<KinematicBody>().enabled = true;
+        }
+    }
+}
